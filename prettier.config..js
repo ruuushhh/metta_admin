0792d011b07a58e8c327da735a5cd38a37cd231d
ruuushhh <ruuushhh2001@gmail.com>
2022-02-13 12:08:11 +0530

    initial setup

---
+++ prettier.config..js
@@ -0,0 +1,13 @@
+module.exports = {
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    arrowParens: 'always',
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    htmlWhitespaceSensitivity: 'css',
+    printWidth: 80,
+  };
+  
